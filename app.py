9cfdbdee9fa3e812780e9099aa654bce626644b9
codebhendi <youseeme9182@gmail.com>
2017-03-07 08:11:47 +0530

    added light code

--- app.py
+++ app.py
@@ -2,6 +2,8 @@
 from flask import Flask, render_template, session, request
 from flask_socketio import SocketIO, emit, join_room, leave_room, \
     close_room, rooms, disconnect
+import RPi.GPIO as GPIO
+import time
 
 # Set this variable to "threading", "eventlet" or "gevent" to test the
 # different async modes, or leave it set to None for the application to choose
@@ -24,15 +26,32 @@ def background_thread():
                       {'data': 'Server generated event', 'count': count},
                       namespace='/test')
 
+def turn_on_light():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(18,GPIO.OUT)
+
+    print "LED on"
+
+    GPIO.output(18,GPIO.HIGH)
+
+    time.sleep(1)
+
+    print "LED off"
+
+    GPIO.output(18,GPIO.LOW)
+
 def process_text(txt_rcvd):
     
     if txt_rcvd == "turn on the light" :
+        turn_on_light()
+
         return "success"
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', async_mode=socketio.async_mode)
+  return render_template('index.html', async_mode=socketio.async_mode)
 
 
 @socketio.on('my_event', namespace='/test')
@@ -57,60 +76,9 @@ def test_broadcast_message(message):
          {'data': message['data'], 'count': session['receive_count']},
          broadcast=True)
 
-
-@socketio.on('join', namespace='/test')
-def join(message):
-    join_room(message['room'])
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': 'In rooms: ' + ', '.join(rooms()),
-          'count': session['receive_count']})
-
-
-@socketio.on('leave', namespace='/test')
-def leave(message):
-    leave_room(message['room'])
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': 'In rooms: ' + ', '.join(rooms()),
-          'count': session['receive_count']})
-
-
-@socketio.on('close_room', namespace='/test')
-def close(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response', {'data': 'Room ' + message['room'] + ' is closing.',
-                         'count': session['receive_count']},
-         room=message['room'])
-    close_room(message['room'])
-
-
-@socketio.on('my_room_event', namespace='/test')
-def send_room_message(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': message['data'], 'count': session['receive_count']},
-         room=message['room'])
-
-
-@socketio.on('disconnect_request', namespace='/test')
-def disconnect_request():
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': 'Disconnected!'})
-    disconnect()
-
-
-@socketio.on('my_ping', namespace='/test')
-def ping_pong():
-    emit('my_pong')
-
-
 @socketio.on('connect', namespace='/test')
 def test_connect():
     global thread
-    #if thread is None:
-    #    thread = socketio.start_background_task(target=background_thread)
     emit('my_response', {'data': 'Connected'})
 
 
